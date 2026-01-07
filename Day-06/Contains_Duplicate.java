9db6fb22158d460a2f32895be0feb7f4b4fbb111
Likhitha HS <likhithahs543@gmail.com>
2026-01-07 23:33:55 +0530

    Contains Duplicate
    Implement solution for LeetCode problem 217 using HashSet.

---
+++ Day-06/Contains_Duplicate.java
@@ -0,0 +1,18 @@
+// LeetCode 217: Contains Duplicate
+// Approach: Hashing (HashSet)
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+        set.add(num);
+        }
+        return false;
+    }
+}
