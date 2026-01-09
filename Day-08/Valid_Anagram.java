1c32f169eaae716d6dda55dc459164b61400eb35
Likhitha HS <likhithahs543@gmail.com>
2026-01-09 21:04:40 +0530

    Valid Anagram
    Implement isAnagram method to check if two strings are anagrams.

---
+++ Day-08/Valid_Anagram.java
@@ -0,0 +1,24 @@
+// LeetCode 242: 
+// Approach: 
+// Time: O()
+// Space: O()
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int c : count){
+            if (c != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
